90a96766025e2fc223778ca30946fb3377d31609
dzecyna <dzecyna@ukr.net>
2019-03-22 13:53:24 +0200

    kkkk

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function($) {
   $('.submenu > li').matchHeight();
+
+});
+$(document).ready(function() {
+  $('.submenu > li').matchHeight();$('img[src*=logo]').fadeTO(4000,0.5);
+
 });
