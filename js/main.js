c2dc8c3c0e7aa4d677a4964f59ac471c9c7528c8
dzecyna <dzecyna@ukr.net>
2019-03-26 15:06:55 +0200

    aaaaaaaaa

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
 
-  $('.submenu > li').matchHeight();
-  $('img[src*=logo]').fadeIn(4000, 0.4);
+    $("#header").on("click", function() {
+        console.log("dzsf")
+        $("#text").slideToggle()
 
-  $().click(function){
-    $(".block-specification").removeClass("active");
-    $(this).toggleClass("active");
-  });
+    });
+
+    let flag = true;
+    $("#button").on("click",function () {
+        console.log(flag);
+
+        if (flag === true) {
+            flag = false;
+
+            $("#menu").slideToggle(500, function () {
+                flag = true;
+            });
+        }
+    });
+
+
+    $("#auth-link").on("click",function(){
+        $('.popup,.popup_overlay').fadeIn(400); //показываем всплывающее окно
+    });
+    $('.closer,.popup_overlay').click(function(){
+        $('.popup,.popup_overlay').fadeOut(400); //скрываем всплывающее окно
+    });
+
+    $("#top").on("click", function() {
+        $("html, body").animate({ scrollTop: 0 }, 3000);
+
+    });
 });
+
